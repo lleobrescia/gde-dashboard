0ad49d4fc3befea425e5869ae894962122682411
Leo Brescia <leo.brescia@pixla.com.br>
2016-11-07 10:23:45 -0200

    lista de turmas

--- app/turmas/turmas.controller.js
+++ app/turmas/turmas.controller.js
@@ -7,16 +7,36 @@
 
   TurmasController.$inject = ['session', 'serverService'];
   function TurmasController(session, serverService) {
+    var idEscola = '577ffe27e371b996be608a62';
     var self = this;
+
+    self.listaTurmas = [];
 
     Activate();
 
     ////////////////
 
-    function Activate() { }
+    function Activate() {
+      GetTurmas();
+    }
 
     function ApagarTurma() { }
 
-    function GetTurmas() { }
+    function GetTurmas() {
+      var endpoint = 'RecuperarDadosTurmasEscola';
+
+      var josonRequest = {
+        'ObjectID': '',
+        'Id_Escola': idEscola
+      };
+
+      serverService.Request(endpoint, josonRequest).then(function (resp) {
+
+        self.listaTurmas = JSON.parse(resp);
+
+        console.log(self.listaTurmas);
+
+      });
+    }
   }
 })();
