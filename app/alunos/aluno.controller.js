1f2865eec4450673673a047701de07c3308fb4f8
Leo Brescia <leo.brescia@pixla.com.br>
2016-11-09 17:32:20 -0200

    Avaliações Pedagógica

--- app/alunos/aluno.controller.js
+++ app/alunos/aluno.controller.js
@@ -9,14 +9,14 @@
     .module('dashboard')
     .controller('AlunoController', AlunoController);
 
-  AlunoController.$inject = ['serverService', 'session', 'toastr', '$state'];
+  AlunoController.$inject = ['serverService', 'session', 'toastr', '$state', 'SortByService'];
 
   /**
   * @namespace AlunoController
   * @desc Adiciona e edita informacoes de um aluno
   * @memberOf Controllers
   */
-  function AlunoController(serverService, session, toastr, $state) {
+  function AlunoController(serverService, session, toastr, $state, SortByService) {
     var self = this;
     var idEscola = '577ffe27e371b996be608a62';
 
@@ -55,6 +55,7 @@
     self.Atualizar = Atualizar;
     self.CancelarEdicao = CancelarEdicao;
     self.MountChart = MountChart;
+    self.SortByService = SortByService //servico usado no view
 
     // RetornarParecerDescritivoPedagogicoAluno
     Activate();
@@ -115,6 +116,7 @@
         var descricao = [];
         var labels = [];
         var nome = resultado.CampoExperiencia;
+        var respostas = [];
 
         angular.forEach(resultado.Itens_Resultado_Avaliacao, function (campos) {
           data.push(campos.Resposta_Value);
@@ -124,18 +126,23 @@
           switch (campos.Resposta_String) {
             case 'Alcançado':
               colors.push('rgb(0, 128, 0)');
+              respostas.push('Alcançado');
               break;
             case 'Em processo':
               colors.push('rgb(255, 255, 0)');
+              respostas.push('Em processo');
               break;
             case 'Acima do esperado':
               colors.push('rgb(0, 0, 255)');
+              respostas.push('Acima do esperado');
               break;
             case 'Não alcançado':
               colors.push('rgb(255, 0, 255)');
+              respostas.push('Não alcançado');
               break;
             case 'Não avaliado':
               colors.push('rgb(255, 0, 0)');
+              respostas.push('Não avaliado');
               break;
 
             default:
@@ -146,6 +153,7 @@
         self.graficos.push({
           'nome': nome,
           'labels': labels,
+          'respostas': respostas,
           'data': data,
           'dataset': {
             'pointBackgroundColor': colors,
@@ -153,6 +161,18 @@
             'backgroundColor': 'rgba(35, 159, 219, 0.4)',
             'borderColor': 'rgb(35, 159, 219)',
             'pointBorderWidth': 10
+          },
+          'options': {
+            scale: {
+              lineArc: true,
+              ticks: {
+                display: false,
+                beginAtZero: true,
+                min: 0,
+                max: 100,
+                fixedStepSize: 25
+              }
+            }
           },
           'descricao': descricao
         });
