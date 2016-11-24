a6f7c399276012e84600a61e539e02841e9690bd
Leo Brescia <leo.brescia@pixla.com.br>
2016-11-24 11:47:55 -0200

    conteudo da lista do calendario

--- app/calendario/calendario.controller.js
+++ app/calendario/calendario.controller.js
@@ -13,21 +13,43 @@
     self.calendario = [];
     self.calendarioSelecionados = [];
 
+    self.Apagar = Apagar;
+    self.ListManagerService = ListManagerService;
+
     Activate();
 
     ////////////////
 
     function Activate() {
       GetDados();
+
+      if ($stateParams.cadastro === 'OK') {
+        toastr.success('Item adicionado');
+      }
     }
 
     function Apagar() {
-      
+      var texto = 'Item Apagado';
+
+      if (self.calendarioSelecionados.length > 1) {
+        texto = 'Itens Apagados';
+      }
+
+      angular.forEach(self.calendarioSelecionados, function (item) {
+        serverService.Request('RemoverItemCalendarioEscolar', { 'ObjectID': item.Id });
+        toastr.success(texto);
+        GetDados();
+      });
     }
 
     function GetDados() {
       serverService.Request('RetornarCalendarioEscolar', { 'ObjectID': '', 'Id_Escola': idEscola }).then(function (resp) {
         console.log(resp);
+        angular.forEach(resp, function (item) {
+          var data = item.Data.split('/');
+          item.Data = new Date(data[2], (data[1] - 1), data[0]);
+        });
+        self.calendario = resp;
       });
     }
   }
